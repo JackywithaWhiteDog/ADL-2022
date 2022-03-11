fc1e4fe3591ea57e44c79d81f4abcc97f6f75cf0
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2022-03-11 01:54:49 +0000

    refact(hw01): add type hint

--- hw01/src/utils/early_stopping.py
+++ hw01/src/utils/early_stopping.py
@@ -1,13 +1,14 @@
 from pytorch_lightning.callbacks import EarlyStopping
+from pytorch_lightning import Trainer, LightningModule
 
 class EarlyStoppingWarmup(EarlyStopping):
-    def __init__(self, warmup=10, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, warmup: int=10, **kwargs):
+        super(EarlyStoppingWarmup, self).__init__(**kwargs)
         self.warmup = warmup
 
-    def on_validation_end(self, trainer, pl_module):
+    def on_validation_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         pass
 
-    def on_epoch_end(self, trainer, pl_module):
+    def on_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         if trainer.current_epoch >= self.warmup:
             super()._run_early_stopping_check(trainer)
