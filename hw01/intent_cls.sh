9b91cc54f1facbcab891801f2b4388810e62bd12
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2022-03-16 13:04:34 +0000

    feat(hw01): accept other flags

--- hw01/intent_cls.sh
+++ hw01/intent_cls.sh
@@ -4,4 +4,4 @@ python intent_test.py \
     --test_file "${1}" \
     --pred_file "${2}" \
     --ckpt_path ckpt/intent/intent-best.ckpt \
-    --device cuda
+    "${@:3}"
--- hw01/slot_tag.sh
+++ hw01/slot_tag.sh
@@ -4,4 +4,4 @@ python slot_test.py \
     --test_file "${1}" \
     --pred_file "${2}" \
     --ckpt_path ckpt/slot/slot-best.ckpt \
-    --device cuda
+    "${@:3}"
