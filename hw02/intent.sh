7d6a881ec885f97e38b081e4065b3393e331fa29
jacky <b07705015@ntu.edu.tw>
2022-04-24 11:45:24 +0800

    feat(hw02): add support for intent classification

---
+++ hw02/intent.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+export CUDA_VISIBLE_DEVICES="4"
+
+python intent.py \
+  --model_name_or_path bert-base-cased \
+  --do_train \
+  --do_eval \
+  --do_predict \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --logging_steps 250 \
+  --evaluation_strategy steps \
+  --eval_steps 250 \
+  --save_strategy epoch \
+  --save_total_limit 1 \
+  --cache_dir ./cache \
+  --train_file ./data/intent/train.json \
+  --validation_file ./data/intent/eval.json \
+  --test_file ./data/intent/test.json \
+  --output_dir ./ckpt/intent
+
+
