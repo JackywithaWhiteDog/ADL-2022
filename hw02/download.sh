b7424abf0dedd39e3b913734dd9e805a5b159529
jacky <b07705015@ntu.edu.tw>
2022-04-24 08:36:51 +0800

    feat(hw02): add support for downloading the pretrained models

---
+++ hw02/download.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+FILE_NAME=hw02.zip
+wget https://www.dropbox.com/s/ptbsin18uhf0khz/hw02.zip?dl=1 -O $FILE_NAME
+unzip $FILE_NAME
--- hw02/run.sh
+++ hw02/run.sh
@@ -2,24 +2,28 @@
 
 export CUDA_VISIBLE_DEVICES="4"
 
+# "${1}": path to the context file.
+# "${2}": path to the testing file.
+# "${3}": path to the output predictions.
+
 python ./test.py \
-  --multi_model ./ckpt/multiple_choice/sample/checkpoint-4000 \
-  --qa_model ./ckpt/qa/roberta/checkpoint-30000 \
-  --per_device_eval_batch_size 16 \
+  --multi_model ./ckpt/multiple_choice \
+  --qa_model ./ckpt/qa \
+  --per_device_eval_batch_size 32 \
   --multi_max_seq_length 512 \
   --qa_max_seq_length 384 \
   --max_answer_length 150 \
   --output_dir . \
   --overwrite_output \
   --cache_dir ./cache \
-  --test_file ./data/test.json \
-  --context_file ./data/context.json \
-  --output_file ./pred/test_prediction.csv
+  --test_file "${2}" \
+  --context_file "${1}" \
+  --output_file "${3}"
 
 # python ./test.py \
-#   --multi_model ./ckpt/multiple_choice/sample/checkpoint-4000 \
-#   --qa_model ./ckpt/qa/roberta/checkpoint-30000 \
-#   --per_device_eval_batch_size 16 \
+#   --multi_model ./ckpt/multiple_choice/roberta/checkpoint-4071 \
+#   --qa_model ./ckpt/qa/roberta_new/checkpoint-38240 \
+#   --per_device_eval_batch_size 32 \
 #   --multi_max_seq_length 512 \
 #   --qa_max_seq_length 384 \
 #   --max_answer_length 150 \
@@ -29,3 +33,45 @@ python ./test.py \
 #   --test_file ./data/valid.json \
 #   --context_file ./data/context.json \
 #   --output_file ./pred/valid_prediction.csv
+
+# python ./test.py \
+#   --multi_model ./ckpt/multiple_choice/albert-tiny/checkpoint-4071 \
+#   --qa_model ./ckpt/qa/roberta_new/checkpoint-38240 \
+#   --per_device_eval_batch_size 32 \
+#   --multi_max_seq_length 512 \
+#   --qa_max_seq_length 384 \
+#   --max_answer_length 150 \
+#   --output_dir . \
+#   --overwrite_output \
+#   --cache_dir ./cache \
+#   --test_file ./data/valid.json \
+#   --context_file ./data/context.json \
+#   --output_file ./pred/valid_prediction.csv
+
+# python ./test.py \
+#   --multi_model ./ckpt/multiple_choice/roberta_no_pretrained/checkpoint-4071 \
+#   --qa_model ./ckpt/qa/roberta_new/checkpoint-38240 \
+#   --per_device_eval_batch_size 32 \
+#   --multi_max_seq_length 512 \
+#   --qa_max_seq_length 384 \
+#   --max_answer_length 150 \
+#   --output_dir . \
+#   --overwrite_output \
+#   --cache_dir ./cache \
+#   --test_file ./data/valid.json \
+#   --context_file ./data/context.json \
+#   --output_file ./pred/valid_prediction.csv
+
+# python ./test.py \
+#   --multi_model ./ckpt/multiple_choice/roberta/checkpoint-4071 \
+#   --qa_model ./ckpt/qa/roberta_512/checkpoint-27680 \
+#   --per_device_eval_batch_size 32 \
+#   --multi_max_seq_length 512 \
+#   --qa_max_seq_length 384 \
+#   --max_answer_length 150 \
+#   --output_dir . \
+#   --overwrite_output \
+#   --cache_dir ./cache \
+#   --test_file ./data/test.json \
+#   --context_file ./data/context.json \
+#   --output_file ./pred/test_prediction_new.csv
